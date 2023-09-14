b2a3f8aa4788db6616967dde5e0dc2d6ae6d1b5a
Bohdana Sherstyniuk <bohdanasherstyniuk@Bohdanas-MacBook-Pro.local>
2023-09-14 16:44:38 +0300

    Initial commit

---
+++ src/main/java/org/bobocode/App.java
@@ -0,0 +1,13 @@
+package org.bobocode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
