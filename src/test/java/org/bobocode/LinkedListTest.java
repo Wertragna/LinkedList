ec3ce5c7b01db17cf30d11322d04c17395477118
Mykhailo_Krechkivsky <Mykhailo_Krechkivskyi@epam.com>
2023-09-14 17:19:31 +0300

    added test class

--- src/test/java/org/bobocode/LinkedListTest.java
+++ src/test/java/org/bobocode/LinkedListTest.java
@@ -4,13 +4,29 @@ package org.bobocode;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+import java.lang.reflect.TypeVariable;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LinkedListTest {
 
     @Test
-    public void LinkedListTestClassExistAndParametrizedByTType() throws ClassNotFoundException {
+    public void linkedListTestClassExists() throws ClassNotFoundException {
+        Class.forName("org.bobocode.linkedlist.LinkedList");
+    }
+
+    @Test
+    public void linkedListTestClassExistsAndParametrizedByTValue() throws ClassNotFoundException {
         Class<?> aClass = Class.forName("org.bobocode.linkedlist.LinkedList");
+        TypeVariable<? extends Class<?>>[] typeParameters = aClass.getTypeParameters();
+        TypeVariable<? extends Class<?>> typeParameter = typeParameters[0];
+        assertThat(typeParameters.length).isEqualTo(1);
+        TypeVariable<? extends Class<?>> firstParam = typeParameters[0];
+        assertThat(firstParam.getName()).isEqualTo("T");
+
+
     }
 
 }
