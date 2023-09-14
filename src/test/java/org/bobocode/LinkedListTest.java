49795f7957503e870708a26a338b68139294a6d1
Mykhailo_Krechkivsky <Mykhailo_Krechkivskyi@epam.com>
2023-09-14 17:03:10 +0300

    added test class

---
+++ src/test/java/org/bobocode/LinkedListTest.java
@@ -0,0 +1,17 @@
+package org.bobocode;
+
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LinkedListTest {
+
+    @Test
+    public void classExistTest() {
+
+    }
+}
