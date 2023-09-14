8e8646317caafd55ec08b1449fa2759d7075b652
Mykhailo_Krechkivsky <Mykhailo_Krechkivskyi@epam.com>
2023-09-14 17:12:30 +0300

    added test class

--- src/test/java/org/bobocode/LinkedListTest.java
+++ src/test/java/org/bobocode/LinkedListTest.java
@@ -1,15 +1,9 @@
 package org.bobocode;
 
 
-import com.sun.xml.internal.ws.policy.AssertionSet;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import java.lang.reflect.TypeVariable;
-
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LinkedListTest {
@@ -17,8 +11,6 @@ public class LinkedListTest {
     @Test
     public void LinkedListTestClassExistAndParametrizedByTType() throws ClassNotFoundException {
         Class<?> aClass = Class.forName("org.bobocode.linkedlist.LinkedList");
-        TypeVariable<? extends Class<?>>[] typeParameters = aClass.getTypeParameters();
-
     }
 
 }
